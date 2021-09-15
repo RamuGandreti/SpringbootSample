2ecb64e9cda67063ce8affb29eb144ecc903efe2
ramubyjava@gmail.com <ramubyjava@gmail.com>
2021-09-15 15:38:30 +0530

    main class code changed

--- src/main/java/in/nareshit/SpringbootSampleApplication.java
+++ src/main/java/in/nareshit/SpringbootSampleApplication.java
@@ -8,7 +8,7 @@ public class SpringbootSampleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootSampleApplication.class, args);
-		System.out.println("@@@@@@###%&&&&&&&");
+		System.out.println("Welcome@@@@");
 	}
 
 }
